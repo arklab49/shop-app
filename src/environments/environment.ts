362b626570ec2c202217382e7fea792fa582f169
Arkadiusz Labudda <arkadiusz.labudda@amway.com>
2018-01-09 11:53:50 +0100

    Backup

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDWSTQoSo4_IlmdEoehw5zULhao62XFoEY",
+    authDomain: "demoshop-a1ff5.firebaseapp.com",
+    databaseURL: "https://demoshop-a1ff5.firebaseio.com",
+    projectId: "demoshop-a1ff5",
+    storageBucket: "demoshop-a1ff5.appspot.com",
+    messagingSenderId: "670392887358"   
+  }
+};
