22cfd3a40474da092cc457d604bc778cc9e2cdd6
Arkadiusz Labudda <arkadiusz.labudda@amway.com>
2018-01-10 17:18:45 +0100

    Creating shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductQuantityComponent } from './components/product-quantity/product-quantity.component';
+import { AdminAuthGuard } from './services/admin-auth-guard.service';
+import { AuthGuard } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { CategoryService } from './services/category.service';
+import { OrderService } from './services/order.service';
+import { ProductService } from './services/product.service';
+import { ShoppingCartService } from './services/shopping-cart.service';
+import { UserService } from './services/user.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ProductCardComponent,
+    ProductQuantityComponent
+  ],
+  exports: [
+    ProductCardComponent,
+    ProductQuantityComponent
+  ],
+   providers: [
+    AuthService,
+    AuthGuard,
+    AdminAuthGuard,
+    UserService,
+    CategoryService,
+    ProductService,
+    ShoppingCartService,
+    OrderService
+  ],
+})
+export class SharedModule { }
