2ce511882a06cbe2b4789bab087524a08a74b442
Arkadiusz Labudda <arkadiusz.labudda@amway.com>
2018-01-11 09:47:46 +0100

    Create Shopping Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AdminModule } from 'admin/admin.module';
-import { AdminAuthGuard } from 'admin/services/admin-auth-guard.service';
 import { DataTableModule } from 'angular-4-data-table';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
@@ -13,41 +12,26 @@ import { CustomFormsModule } from 'ng2-validation';
 import { SharedModule } from 'shared/shared.module';
 
 import { environment } from './../environments/environment';
-import { ProductFormComponent } from './admin/components/product-form/product-form.component';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { CheckOutComponent } from './check-out/check-out.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { ProductFilterComponent } from './products/product-filter/product-filter.component';
-import { ProductsComponent } from './products/products.component';
-import { AuthGuard } from './shared/services/auth-guard.service';
-import { ShippingFormComponent } from './shipping-form/shipping-form.component';
-import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './shopping/components/products/products.component';
+import { ShoppingModule } from 'shopping/shopping.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     BsNavbarComponent,
     HomeComponent,
-    ProductsComponent,
-    ShoppingCartComponent,
-    CheckOutComponent,
-    OrderSuccessComponent,
-    MyOrdersComponent,
-    LoginComponent,
-    ProductFilterComponent,
-    ShoppingCartSummaryComponent,
-    ShippingFormComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AdminModule,
     FormsModule,
+    ShoppingModule,
     CustomFormsModule,
     DataTableModule,
     AngularFireModule.initializeApp(environment.firebase),
@@ -56,19 +40,7 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
     NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: ProductsComponent },
-      { path: 'products', component: ProductsComponent },
-      { path: 'shopping-cart', component: ShoppingCartComponent },
-      { path: 'login', component: LoginComponent },
-
-      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
-      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard] },
-      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
-
-      {
-        path: 'admin/products/new',
-        component: ProductFormComponent,
-        canActivate: [AuthGuard, AdminAuthGuard]
-      }
+      { path: 'login', component: LoginComponent }
     ])
   ],
   providers: [],
---
+++ src/app/shopping/shopping.module.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from 'shared/services/auth-guard.service';
+import { OrderSuccessComponent } from 'shopping/components/order-success/order-success.component';
+
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
+import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from 'shared/shared.module';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductsComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+
+      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
+      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard] },
+      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
+    ])
+  ],
+  declarations: [
+    ProductFilterComponent,
+    ShoppingCartSummaryComponent,
+    ShippingFormComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+  ]
+})
+export class ShoppingModule { }
